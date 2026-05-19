70c2d4899fcbca72225c36f5d10913c32bf9b5f8
Nikeshnikku <55690082+nikeshnikku9@users.noreply.github.com>
2026-05-19 20:22:03 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Nimad ZAYKA',
+  description: 'Premium Indian Spices',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
