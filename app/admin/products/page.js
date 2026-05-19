17760df9714710aeadab41fb77ce2ddc56428220
Nikeshnikku <55690082+nikeshnikku9@users.noreply.github.com>
2026-05-20 02:08:24 +0530

    Update page.js

--- app/admin/products/page.js
+++ app/admin/products/page.js
@@ -8,43 +8,177 @@ export default function ProductsPage() {
 
     {
       id: 1,
-      name: "Haldi Powder",
-      size: "100g",
+      name: "Meat Masala",
+      size: "20g",
       price: 45,
       stock: 120,
-      barcode: "920100000016"
+      barcode: "920100000001"
     },
 
     {
       id: 2,
-      name: "Mirchi Powder",
-      size: "100g",
-      price: 65,
+      name: "Meat Masala",
+      size: "50g",
+      price: 90,
       stock: 80,
-      barcode: "920100000021"
+      barcode: "920100000002"
     },
 
     {
       id: 3,
-      name: "Dhaniya Powder",
+      name: "Meat Masala",
       size: "100g",
-      price: 55,
-      stock: 95,
-      barcode: "920100000026"
+      price: 160,
+      stock: 50,
+      barcode: "920100000003"
     },
 
     {
       id: 4,
-      name: "Meat Masala",
+      name: "Garam Masala",
       size: "50g",
-      price: 90,
+      price: 85,
+      stock: 100,
+      barcode: "920100000004"
+    },
+
+    {
+      id: 5,
+      name: "Garam Masala",
+      size: "100g",
+      price: 150,
+      stock: 60,
+      barcode: "920100000005"
+    },
+
+    {
+      id: 6,
+      name: "Shahi Paneer Masala",
+      size: "50g",
+      price: 95,
+      stock: 75,
+      barcode: "920100000006"
+    },
+
+    {
+      id: 7,
+      name: "Shahi Paneer Masala",
+      size: "100g",
+      price: 170,
+      stock: 45,
+      barcode: "920100000007"
+    },
+
+    {
+      id: 8,
+      name: "Dal Bati Masala",
+      size: "50g",
+      price: 80,
+      stock: 90,
+      barcode: "920100000008"
+    },
+
+    {
+      id: 9,
+      name: "Dal Bati Masala",
+      size: "100g",
+      price: 145,
+      stock: 55,
+      barcode: "920100000009"
+    },
+
+    {
+      id: 10,
+      name: "Khada Masala",
+      size: "50g",
+      price: 110,
       stock: 40,
-      barcode: "920100000002"
+      barcode: "920100000010"
+    },
+
+    {
+      id: 11,
+      name: "Khada Masala",
+      size: "100g",
+      price: 200,
+      stock: 25,
+      barcode: "920100000011"
+    },
+
+    {
+      id: 12,
+      name: "Chicken Masala",
+      size: "20g",
+      price: 40,
+      stock: 120,
+      barcode: "920100000012"
+    },
+
+    {
+      id: 13,
+      name: "Chicken Masala",
+      size: "50g",
+      price: 85,
+      stock: 80,
+      barcode: "920100000013"
+    },
+
+    {
+      id: 14,
+      name: "Chicken Masala",
+      size: "100g",
+      price: 155,
+      stock: 50,
+      barcode: "920100000014"
+    },
+
+    {
+      id: 15,
+      name: "Haldi Powder",
+      size: "50g",
+      price: 25,
+      stock: 200,
+      barcode: "920100000015"
+    },
+
+    {
+      id: 16,
+      name: "Haldi Powder",
+      size: "100g",
+      price: 45,
+      stock: 180,
+      barcode: "920100000016"
+    },
+
+    {
+      id: 17,
+      name: "Haldi Powder",
+      size: "200g",
+      price: 80,
+      stock: 140,
+      barcode: "920100000017"
+    },
+
+    {
+      id: 18,
+      name: "Haldi Powder",
+      size: "500g",
+      price: 180,
+      stock: 90,
+      barcode: "920100000018"
+    },
+
+    {
+      id: 19,
+      name: "Haldi Powder",
+      size: "1kg",
+      price: 320,
+      stock: 40,
+      barcode: "920100000019"
     }
 
   ])
 
-  // ADD PRODUCT
   const addProduct = () => {
 
     const name = prompt("Enter Product Name")
@@ -77,7 +211,6 @@ export default function ProductsPage() {
 
   }
 
-  // EDIT PRODUCT
   const editProduct = (id) => {
 
     const updatedProducts = products.map((item) => {
@@ -112,7 +245,6 @@ export default function ProductsPage() {
 
   }
 
-  // DELETE PRODUCT
   const deleteProduct = (id) => {
 
     const filteredProducts = products.filter(
@@ -133,8 +265,6 @@ export default function ProductsPage() {
         color: "white"
       }}
     >
-
-      {/* TOP HEADER */}
 
       <div
         style={{
@@ -179,8 +309,6 @@ export default function ProductsPage() {
 
       </div>
 
-      {/* COMPANY DETAILS */}
-
       <div
         style={{
           background: "#5a2414",
@@ -214,8 +342,6 @@ export default function ProductsPage() {
         </p>
 
       </div>
-
-      {/* PRODUCTS GRID */}
 
       <div
         style={{
