53e3e3ad1589fd9b4246b07da86eef44cac4564b
Nikeshnikku <55690082+nikeshnikku9@users.noreply.github.com>
2026-05-20 02:16:55 +0530

    Update page.js

--- app/admin/products/page.js
+++ app/admin/products/page.js
@@ -6,8 +6,147 @@ export default function ProductsPage() {
 
   const defaultProducts = [
 
+    // PREMIUM BOX PACKAGING
+
     {
       id: 1,
+      name: "Meat Masala",
+      size: "20g",
+      price: 45,
+      stock: 120,
+      barcode: "920100000001"
+    },
+
+    {
+      id: 2,
+      name: "Meat Masala",
+      size: "50g",
+      price: 90,
+      stock: 80,
+      barcode: "920100000002"
+    },
+
+    {
+      id: 3,
+      name: "Meat Masala",
+      size: "100g",
+      price: 160,
+      stock: 50,
+      barcode: "920100000003"
+    },
+
+    {
+      id: 4,
+      name: "Garam Masala",
+      size: "50g",
+      price: 85,
+      stock: 100,
+      barcode: "920100000004"
+    },
+
+    {
+      id: 5,
+      name: "Garam Masala",
+      size: "100g",
+      price: 150,
+      stock: 60,
+      barcode: "920100000005"
+    },
+
+    {
+      id: 6,
+      name: "Shahi Paneer Masala",
+      size: "50g",
+      price: 95,
+      stock: 75,
+      barcode: "920100000006"
+    },
+
+    {
+      id: 7,
+      name: "Shahi Paneer Masala",
+      size: "100g",
+      price: 170,
+      stock: 45,
+      barcode: "920100000007"
+    },
+
+    {
+      id: 8,
+      name: "Dal Bati Masala",
+      size: "50g",
+      price: 80,
+      stock: 90,
+      barcode: "920100000008"
+    },
+
+    {
+      id: 9,
+      name: "Dal Bati Masala",
+      size: "100g",
+      price: 145,
+      stock: 55,
+      barcode: "920100000009"
+    },
+
+    {
+      id: 10,
+      name: "Khada Masala",
+      size: "50g",
+      price: 110,
+      stock: 40,
+      barcode: "920100000010"
+    },
+
+    {
+      id: 11,
+      name: "Khada Masala",
+      size: "100g",
+      price: 200,
+      stock: 25,
+      barcode: "920100000011"
+    },
+
+    {
+      id: 12,
+      name: "Chicken Masala",
+      size: "20g",
+      price: 40,
+      stock: 120,
+      barcode: "920100000012"
+    },
+
+    {
+      id: 13,
+      name: "Chicken Masala",
+      size: "50g",
+      price: 85,
+      stock: 80,
+      barcode: "920100000013"
+    },
+
+    {
+      id: 14,
+      name: "Chicken Masala",
+      size: "100g",
+      price: 155,
+      stock: 50,
+      barcode: "920100000014"
+    },
+
+    // STANDARD PLASTIC PACKAGING
+
+    {
+      id: 15,
+      name: "Haldi Powder",
+      size: "50g",
+      price: 25,
+      stock: 200,
+      barcode: "920100000015"
+    },
+
+    {
+      id: 16,
       name: "Haldi Powder",
       size: "100g",
       price: 45,
@@ -16,19 +155,171 @@ export default function ProductsPage() {
     },
 
     {
-      id: 2,
+      id: 17,
+      name: "Haldi Powder",
+      size: "200g",
+      price: 80,
+      stock: 140,
+      barcode: "920100000017"
+    },
+
+    {
+      id: 18,
+      name: "Haldi Powder",
+      size: "500g",
+      price: 180,
+      stock: 90,
+      barcode: "920100000018"
+    },
+
+    {
+      id: 19,
+      name: "Haldi Powder",
+      size: "1kg",
+      price: 320,
+      stock: 40,
+      barcode: "920100000019"
+    },
+
+    {
+      id: 20,
+      name: "Mirchi Powder",
+      size: "50g",
+      price: 35,
+      stock: 180,
+      barcode: "920100000020"
+    },
+
+    {
+      id: 21,
       name: "Mirchi Powder",
       size: "100g",
       price: 65,
       stock: 160,
       barcode: "920100000021"
+    },
+
+    {
+      id: 22,
+      name: "Mirchi Powder",
+      size: "200g",
+      price: 120,
+      stock: 120,
+      barcode: "920100000022"
+    },
+
+    {
+      id: 23,
+      name: "Mirchi Powder",
+      size: "500g",
+      price: 280,
+      stock: 70,
+      barcode: "920100000023"
+    },
+
+    {
+      id: 24,
+      name: "Mirchi Powder",
+      size: "1kg",
+      price: 520,
+      stock: 30,
+      barcode: "920100000024"
+    },
+
+    {
+      id: 25,
+      name: "Dhaniya Powder",
+      size: "50g",
+      price: 30,
+      stock: 170,
+      barcode: "920100000025"
+    },
+
+    {
+      id: 26,
+      name: "Dhaniya Powder",
+      size: "100g",
+      price: 55,
+      stock: 150,
+      barcode: "920100000026"
+    },
+
+    {
+      id: 27,
+      name: "Dhaniya Powder",
+      size: "200g",
+      price: 100,
+      stock: 100,
+      barcode: "920100000027"
+    },
+
+    {
+      id: 28,
+      name: "Dhaniya Powder",
+      size: "500g",
+      price: 220,
+      stock: 60,
+      barcode: "920100000028"
+    },
+
+    {
+      id: 29,
+      name: "Dhaniya Powder",
+      size: "1kg",
+      price: 400,
+      stock: 25,
+      barcode: "920100000029"
+    },
+
+    {
+      id: 30,
+      name: "Garam Masala",
+      size: "50g",
+      price: 40,
+      stock: 150,
+      barcode: "920100000030"
+    },
+
+    {
+      id: 31,
+      name: "Garam Masala",
+      size: "100g",
+      price: 75,
+      stock: 120,
+      barcode: "920100000031"
+    },
+
+    {
+      id: 32,
+      name: "Garam Masala",
+      size: "200g",
+      price: 140,
+      stock: 80,
+      barcode: "920100000032"
+    },
+
+    {
+      id: 33,
+      name: "Garam Masala",
+      size: "500g",
+      price: 320,
+      stock: 45,
+      barcode: "920100000033"
+    },
+
+    {
+      id: 34,
+      name: "Garam Masala",
+      size: "1kg",
+      price: 580,
+      stock: 20,
+      barcode: "920100000034"
     }
 
-  ]
+  ])
 
   const [products, setProducts] = useState([])
 
-  // LOAD PRODUCTS
   useEffect(() => {
 
     const savedProducts =
@@ -46,7 +337,6 @@ export default function ProductsPage() {
 
   }, [])
 
-  // SAVE PRODUCTS
   useEffect(() => {
 
     if (products.length > 0) {
@@ -60,7 +350,6 @@ export default function ProductsPage() {
 
   }, [products])
 
-  // ADD PRODUCT
   const addProduct = () => {
 
     const name =
@@ -106,7 +395,6 @@ export default function ProductsPage() {
 
   }
 
-  // EDIT PRODUCT
   const editProduct = (id) => {
 
     const updatedProducts =
@@ -142,7 +430,6 @@ export default function ProductsPage() {
 
   }
 
-  // DELETE PRODUCT
   const deleteProduct = (id) => {
 
     const filteredProducts =
