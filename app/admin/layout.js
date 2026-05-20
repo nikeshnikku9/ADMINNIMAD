ec4e1a9d099c883e67f95b47c17af8662a061c15
Nikeshnikku <55690082+nikeshnikku9@users.noreply.github.com>
2026-05-20 15:39:12 +0530

    Update layout.js

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,64 +1,22 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { usePathname, useRouter } from "next/navigation";
+export default function AdminLayout({
+  children,
+}) {
 
-export default function AdminLayout({ children }) {
+  return (
 
-  const router = useRouter();
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#1a0d08",
+      }}
+    >
 
-  const pathname = usePathname();
+      {children}
 
-  const [allowed, setAllowed] = useState(false);
+    </div>
 
-  useEffect(() => {
-
-    // LOGIN PAGE KO ALLOW KARO
-    if (pathname === "/admin") {
-
-      setAllowed(true);
-
-      return;
-
-    }
-
-    // LOGIN CHECK
-    const login =
-      localStorage.getItem("nimad-admin-login");
-
-    if (login === "true") {
-
-      setAllowed(true);
-
-    } else {
-
-      router.push("/admin");
-
-    }
-
-  }, [pathname]);
-
-  if (!allowed) {
-
-    return (
-      <div
-        style={{
-          minHeight: "100vh",
-          background: "#1a0d08",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          color: "white",
-          fontSize: "24px",
-          fontWeight: "bold",
-        }}
-      >
-        Loading...
-      </div>
-    );
-
-  }
-
-  return children;
+  );
 
 }
